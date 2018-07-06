3444de88e01b47b2853195e49232c637fa70f5f2
Zac McClung <zac.mcclung370@gmail.com>
2018-07-05 21:28:35 -0600

    afterEach statements

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import { mount } from 'enzyme';
 import CommentBox from 'components/CommentBox';
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+afterEach(() => {
+  wrapped.unmount();
+});
+
 it('has a text area and a button', () => {
   const wrapped = mount(<CommentBox />);
 
