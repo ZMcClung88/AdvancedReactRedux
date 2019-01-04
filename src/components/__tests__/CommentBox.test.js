e2f2181d6d11cfaa1d5c8a158aef2bea91f9b6c7
Zac McClung <zac.mcclung370@gmail.com>
2019-01-03 21:36:50 -0500

    saving changes

--- src/components/__tests__/CommentBox.test.js
+++ src/components/__tests__/CommentBox.test.js
@@ -19,23 +19,22 @@ it('has a text area and a button', () => {
   expect(wrapped.find('button').length).toEqual(1);
 });
 
-it('has a text area that users can type in', () => {
-  wrapped.find('textarea').simulate('change', {
-    target: { value: 'new comment' }
+describe('the text area', () => {
+  beforeEach(() => {
+    wrapped.find('textarea').simulate('change', {
+      target: { value: 'new comment' }
+    });
+
+    wrapped.update();
   });
 
-  wrapped.update();
+  it('has a text area that users can type in', () => {
+    expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  });
 
-  expect(wrapped.find('textarea').prop('value')).toEqual('new comment');
+  it('submits and textarea is emptied', () => {
+    wrapped.find('form').simulate('submit');
+    wrapped.update();
+    expect(wrapped.find('textarea').prop('value')).toEqual('');
+  });
 });
-
-// it('submits and textarea is emptied', () => {
-//   wrapped.find('textarea').simulate('change', {
-//     target: { value: 'new comment' }
-//   });
-//   wrapped.update();
-//
-//   wrapped.find('form').simulate('submit');
-//   wrapped.update();
-//   expect(wrapped.find('textarea').prop('value')).toEqual('');
-// });
